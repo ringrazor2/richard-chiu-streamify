42d1588eec83a94a073a7d67bfcd85da758ddc9f
Richard Chiu <richard.chiu87@gmail.com>
2023-03-12 10:42:32 -0400

    h1 message added

---
+++ src/components/HomeMessage/HomeMessage.js
@@ -0,0 +1,13 @@
+import "./HomeMessage.scss";
+
+const HomeMessage = () => {
+  return (
+    <>
+      <div className="homeMessage-container">
+        <h1 className="homeMessage">Find your show now!</h1>
+      </div>
+    </>
+  );
+};
+
+export default HomeMessage;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import "./HomePage.scss";
 import NavBar from "../../components/NavBar/NavBar";
-
+import HomeMessage from "../../components/HomeMessage/HomeMessage";
 const HomePage = () => {
   return (
     <>
       <div className="homePage">
         <NavBar />
+        <HomeMessage className="homePage-textContainer" />
       </div>
     </>
   );
