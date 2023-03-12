0572cbc102411def16a3252fdce8446e491a3eeb
Richard Chiu <richard.chiu87@gmail.com>
2023-03-12 00:44:50 -0500

    logos added to nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,33 +1,43 @@
 import "./NavBar.scss";
 import { NavLink } from "react-router-dom";
+import home from "../../assets/images/icons/home-icon.png";
+import search from "../../assets/images/icons/search-icon.png";
+import chat from "../../assets/images/icons/chat-icon.png";
+import list from "../../assets/images/icons/list-icon.png";
 const NavBar = () => {
   return (
     <>
       <nav className="navBar">
         <ul className="navBar__links">
-          STREAMIFY
-          <li className="navBar__link">
-            <NavLink className="navBar__anchor" href="/">
-              <img src="../../assets/images/icons/home-icon.png"></img>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="navBar__anchor" href="/">
-              <img src="../../assets/images/icons/search-icon.png"></img>Search
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="navBar__anchor" href="/">
-              <img src="../../assets/images/icons/chat-icon.png"></img>
-              Chat
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className="navBar__anchor" href="#">
-              Lists
-            </NavLink>
-          </li>
+          <div className="navBar__sep">
+            <h1 className="site__title">STREAMIFY</h1>
+
+            <div>
+              <li>
+                <NavLink className="navBar__anchor" href="/">
+                  <img src={home}></img>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navBar__anchor" href="/">
+                  <img src={search}></img>
+                  Search
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navBar__anchor" href="/">
+                  <img src={chat}></img>
+                  Chat
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navBar__anchor" href="#">
+                  <img src={list}></img>Lists
+                </NavLink>
+              </li>
+            </div>
+          </div>
         </ul>
       </nav>
     </>
