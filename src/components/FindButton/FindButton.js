8cb5e6c1a96a3299a6cee344906528c52040bb6f
Richard Chiu <richard.chiu87@gmail.com>
2023-03-12 13:12:27 -0400

    FindButton component and styling template done

---
+++ src/components/FindButton/FindButton.js
@@ -0,0 +1,11 @@
+import "./FindButton.scss";
+
+const FindButton = () => {
+  return (
+    <button className="custom-btn btn-8">
+      <span>Read More</span>
+    </button>
+  );
+};
+
+export default FindButton;
