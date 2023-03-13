c0725110bfb3c5ac07100a86b82f40fe5732a3b3
Richard Chiu <richard.chiu87@gmail.com>
2023-03-12 20:50:49 -0400

    explore button styling done

---
+++ src/components/ExploreButton/ExploreButton.js
@@ -0,0 +1,11 @@
+import "./ExploreButton.scss";
+
+const ExportButton = () => {
+  return (
+    <button className="explore-button">
+      <span className="explore-button__text">Explore</span>
+    </button>
+  );
+};
+
+export default ExportButton;
--- src/components/HomeMessage/HomeMessage.js
+++ src/components/HomeMessage/HomeMessage.js
@@ -1,10 +1,12 @@
 import "./HomeMessage.scss";
+import ExploreButton from "../ExploreButton/ExploreButton";
 
 const HomeMessage = () => {
   return (
     <>
       <div className="homeMessage-container">
         <h1 className="homeMessage">Find your show now!</h1>
+        <ExploreButton />
       </div>
     </>
   );
