a0077aec3f9a446a0ccb997cd36782cd6da794c8
Richard Chiu <richard.chiu87@gmail.com>
2023-03-13 12:58:12 -0400

    color scheme for nav changed, icons bigger

--- src/components/StreamingLogos/StreamingLogos.js
+++ src/components/StreamingLogos/StreamingLogos.js
@@ -1,4 +1,5 @@
 import "./StreamingLogos.scss";
+import { Link } from "react-router-dom";
 import netflix from "../../assets/images/icons/netflix-icon.png";
 import primeVideo from "../../assets/images/icons/primevideo-icon.png";
 import disneyPlus from "../../assets/images/icons/disneyPlus-icon.png";
@@ -7,19 +8,29 @@ import hbo from "../../assets/images/icons/hbo-icon.png";
 const StreamingLogos = () => {
   return (
     <div className="streaming-logos">
-      <img className="streaming-logo" alt="netflix logo" src={netflix}></img>
-      <img
-        className="streaming-logo"
-        alt="prime video logo"
-        src={primeVideo}
-      ></img>
-      <img
-        className="streaming-logo"
-        alt="disney plus logo"
-        src={disneyPlus}
-      ></img>
-      <img className="streaming-logo" alt="crave logo" src={crave}></img>
-      <img className="streaming-logo" alt="hbo logo" src={hbo}></img>
+      <Link to="www.netflix.com">
+        <img className="streaming-logo" alt="netflix logo" src={netflix}></img>
+      </Link>
+      <Link to="www.primevideo.com">
+        <img
+          className="streaming-logo"
+          alt="prime video logo"
+          src={primeVideo}
+        ></img>
+      </Link>
+      <Link to="www.disneyplus.com">
+        <img
+          className="streaming-logo"
+          alt="disney plus logo"
+          src={disneyPlus}
+        ></img>
+      </Link>
+      <Link to="www.crave.com">
+        <img className="streaming-logo" alt="crave logo" src={crave}></img>
+      </Link>
+      <Link to="www.hbomax.com">
+        <img className="streaming-logo" alt="hbo logo" src={hbo}></img>
+      </Link>
     </div>
   );
 };
