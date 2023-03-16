d780f1b85b864870e30bb2942b756cfbe6553122
Richard Chiu <richard.chiu87@gmail.com>
2023-03-16 11:35:42 -0400

    more messages added to home message banner

--- src/components/HomeMessage/HomeMessage.js
+++ src/components/HomeMessage/HomeMessage.js
@@ -5,7 +5,15 @@ const HomeMessage = () => {
   return (
     <>
       <div className="homeMessage-container">
-        <h1 className="homeMessage">Find your show now!</h1>
+        <div className = "homeMessage__title-container">
+        <h1 className = "homeMessage__title">Making</h1>
+        <h1 className = "homeMessage__title">Streaming Easier!</h1>
+       </div>
+        <p className = "homeMessage__text">Trying to figure out where to watch your show? We got you!</p>
+        <p className = "homeMessage__text-quality">Streaming Service</p>
+        <p className = "homeMessage__text-quality">Region</p>
+        <p className = "homeMessage__text-quality">Ratings</p>
+        <h1 className="homeMessage__explore">Find your show now!</h1>
         <ExploreButton />
         <StreamingLogos />
       </div>
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import "./HomePage.scss";
 import NavBar from "../../components/NavBar/NavBar";
 import HomeMessage from "../../components/HomeMessage/HomeMessage";
+import Slider from "../../components/Slider/Slider";
 const HomePage = () => {
   return (
     <>
       <div className="homePage">
         <NavBar />
         <HomeMessage className="homePage-textContainer" />
+        {/* <Slider /> */}
       </div>
     </>
   );
