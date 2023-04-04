9232795dabe7f0e67490786bdcb463286814b4d8
Richard Chiu <richard.chiu87@gmail.com>
2023-04-04 00:05:33 -0400

    data fetch for fave, and watchlist done

--- src/components/SavedShow/SavedShow.js
+++ src/components/SavedShow/SavedShow.js
@@ -1,24 +1,86 @@
-import { useState } from "react";
 import "./SavedShow.scss";
+import { Link } from "react-router-dom";
+import trash from "../../assets/images/icons/trash-icon.png";
+import notFound from "../../assets/images/icons/notfound-icon.png";
+import netflix from "../../assets/images/icons/netflix-icon.png";
+import prime from "../../assets/images/icons/primevideo-icon.png";
+import disneyPlus from "../../assets/images/icons/disneyPlus-icon.png";
+import crave from "../../assets/images/icons/crave-icon.png";
+import hbo from "../../assets/images/icons/hbo-icon.png";
+import hulu from "../../assets/images/icons/hulu-icon.png";
+import crunchyroll from "../../assets/images/icons/crunchyroll-icon.png";
+import peacock from "../../assets/images/icons/peakcock-icon.png";
+import apple from "../../assets/images/icons/apple-icon.webp";
 
-const SavedShow = ({ show }) => {
-  const [savedShows, setSavedShows] = useState([]);
-  //   return (
-  //     {savedShows.length === 0? (<p>You don't have any shows saved. Please add a show to your watch list</p>):(  <div className="saved-show">
-  //     <div className="saved-show__image-container">
-  //       <img
-  //         className="saved-show__image"
-  //         src={show.image.original}
-  //         alt="show poster"
-  //       />
-  //     </div>
-  //     <div className="saved-show__info">
-  //       <h3 className="saved-show__title">{show.name}</h3>
-  //       <p className="saved-show__summary">{show.summary}</p>
-  //     </div>
-  //   </div>) }
-
-  //   );
+const SavedShow = ({ show, deleteShow }) => {
+  return (
+    <div className="show-card">
+      <img
+        className="show-card__image"
+        src={show.posterURLs[342]}
+        alt="show poster"
+      />
+      <div className="show-card__right">
+        <div className="show-card__description-container">
+          <div className="show-card__heading-container">
+            <h4 className="show-card__title">{show.title}</h4>
+            <img
+              src={trash}
+              className="trash-icon"
+              id={show.imdbId}
+              onClick={deleteShow(show.imdbId)}
+            ></img>
+          </div>
+          <p className="show-card__overview">{show.overview}</p>
+        </div>
+        <div className="show-card__streamLogos">
+          {show.streamingService &&
+            show.streamingService.map((service, index) => {
+              let iconSrc = "";
+              let linkSrc = "";
+              if (service === "netflix") {
+                iconSrc = netflix;
+                linkSrc = show.streamingInfo.us.netflix[0].link;
+              } else if (service === "prime") {
+                iconSrc = prime;
+                linkSrc = show.streamingInfo.us.prime[0].link;
+              } else if (service === "hbo") {
+                iconSrc = hbo;
+                linkSrc = show.streamingInfo.us.hbo[0].link;
+              } else if (service === "crave") {
+                iconSrc = crave;
+                linkSrc = show.streamingInfo.us.crave[0].link;
+              } else if (service === "hulu") {
+                iconSrc = hulu;
+                linkSrc = show.streamingInfo.us.hulu[0].link;
+              } else if (service === "disney") {
+                iconSrc = disneyPlus;
+                linkSrc = show.streamingInfo.us.disney[0].link;
+              } else if (service === "crunchyroll") {
+                iconSrc = crunchyroll;
+                linkSrc = show.streamingInfo.us.crunchyroll[0].link;
+              } else if (service === "peacock") {
+                iconSrc = peacock;
+                linkSrc = show.streamingInfo.us.peacock[0].link;
+              } else if (service === "apple") {
+                iconSrc = apple;
+                linkSrc = show.streamingInfo.us.apple[0].link;
+              }
+              return (
+                <Link target="_blank" to={linkSrc}>
+                  <img
+                    key={index}
+                    className="show-card__stream-icon"
+                    src={iconSrc || notFound}
+                    alt={`${service} icon`}
+                  />
+                </Link>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SavedShow;
--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -2,25 +2,15 @@ import { useState, useEffect } from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase";
-import {
-  collection,
-  getDocs,
-  getDoc,
-  doc,
-  onSnapshot,
-  updateDoc,
-} from "firebase/firestore";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 import SavedShow from "../../components/SavedShow/SavedShow";
 import "./Account.scss";
-// import { db } from "../../firebase";
 
 const Account = () => {
-  const [users, setUsers] = useState([]);
-  const [faveList, setFaveList] = useState([]);
-  const [watchList, setWatchList] = useState([]);
-  const [threeByThree, setThreeByThree] = useState([]);
-
-  // const userPath = doc(db, "users", `${user.email}`);
+  const [faveList, setFaveList] = useState(null);
+  const [watchList, setWatchList] = useState(null);
+  const [threeByThree, setThreeByThree] = useState(null);
+  const [activeList, setActiveList] = useState(false);
 
   const { logOut, user } = UserAuth();
   const navigate = useNavigate();
@@ -33,40 +23,27 @@ const Account = () => {
     }
   };
 
-  const docRef = doc(db, "users", `${user?.email}`);
-  console.log(docRef);
-  const docSnap = getDoc(docRef).then((doc) => {
-    console.log(doc);
-  });
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user.email}`), (doc) => {
+      setFaveList(doc.data()?.faveList);
+      setWatchList(doc.data()?.watchList);
+      setThreeByThree(doc.data()?.threeByThree);
+    });
+  }, []);
 
-  // const usersRef = collection(db, "users");
-  // const getUserData = async () => {
-  //   const faveData = await getDocs(usersRef);
-  //   console.log(faveData);
-  // };
-  // getUserData();
-
-  // useEffect(() => {
-  //   const docRef = doc(db, "users", `${user?.email}`);
-  //   console.log(docRef);
-  //   const docSnap = await getDoc(docRef);
-  //   console.log(docSnap);
-  // }, [user.email]);
-  // useEffect(() => {
-  //   onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
-  //     console.log(doc.data.faveList);
-  //     setFaveList(doc.data().faveList);
-  //   });
-  // }, [user.email]);
-
-  // useEffect(() => {
-  //   const getUsers = async () => {
-  //     const data = await getDocs(usersRef);
-  //     console.log(data);
-  //     setUsers(data.docs.map((doc) => ({ ...doc.data() })));
-  //   };
-  //   getUsers();
-  // }, []);
+  if (!faveList || !watchList || !threeByThree) return <div>Loading...</div>;
+  const showPath = doc(db, "users", `${user.email}`);
+  const deleteShow = async (id) => {
+    try {
+      const result = faveList.filter((show) => show.imdbId !== id);
+      await updateDoc(showPath, {
+        favList: result,
+      });
+    } catch {
+      console.log("Error deleting show");
+    }
+  };
+  console.log(faveList);
   return (
     <div className="account-page">
       <div className="account-page__details">
@@ -82,14 +59,51 @@ const Account = () => {
         </div>
 
         <div className="account-page__profile-container">
-          <button className="signout-button">Favourites</button>
-          <button className="signout-button">Watch List</button>
-          <button className="signout-button">3x3</button>
+          <button
+            className={
+              activeList ? " signout-button button-active" : "signout-button"
+            }
+            onClick={() => setActiveList(true)}
+          >
+            Favourites
+          </button>
+          <button
+            className={
+              !activeList ? " signout-button button-active" : "signout-button"
+            }
+            onClick={() => setActiveList(false)}
+          >
+            Watch List
+          </button>
+          <button className="signout-button grid-button">3x3</button>
         </div>
       </div>
-      <div className="profile-lists">
-        <div className="show-card"></div>
-      </div>
+      {activeList && faveList !== null && (
+        <div className="profile-lists">
+          {faveList.map((show) => {
+            return (
+              <SavedShow
+                show={show}
+                key={show.imdbId}
+                deleteShow={deleteShow}
+              />
+            );
+          })}
+        </div>
+      )}
+      {!activeList && watchList !== null && (
+        <div className="profile-lists">
+          {watchList.map((show) => {
+            return (
+              <SavedShow
+                show={show}
+                key={show.imdbId}
+                deleteShow={deleteShow}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
