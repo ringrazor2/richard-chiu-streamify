0cca72796d0485beb41558582f4479e636df599b
Richard Chiu <richard.chiu87@gmail.com>
2023-04-03 20:45:52 -0400

    getting data from db

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -2,14 +2,26 @@ import { useState, useEffect } from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { db } from "../../firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
 import SavedShow from "../../components/SavedShow/SavedShow";
-
 import "./Account.scss";
 // import { db } from "../../firebase";
-// import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 const Account = () => {
+  const [users, setUsers] = useState([]);
+  const [faveList, setFaveList] = useState([]);
+  const [watchList, setWatchList] = useState([]);
+  const [threeByThree, setThreeByThree] = useState([]);
+
+  // const userPath = doc(db, "users", `${user.email}`);
+
   const { logOut, user } = UserAuth();
   const navigate = useNavigate();
   const handleSignOut = async () => {
@@ -21,16 +33,40 @@ const Account = () => {
     }
   };
 
-  const [users, setUsers] = useState([]);
-  const usersRef = collection(db, "users");
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersRef);
-      console.log(data);
-      setUsers(data.docs.map((doc) => ({ ...doc.data() })));
-    };
-    getUsers();
-  }, []);
+  const docRef = doc(db, "users", `${user?.email}`);
+  console.log(docRef);
+  const docSnap = getDoc(docRef).then((doc) => {
+    console.log(doc);
+  });
+
+  // const usersRef = collection(db, "users");
+  // const getUserData = async () => {
+  //   const faveData = await getDocs(usersRef);
+  //   console.log(faveData);
+  // };
+  // getUserData();
+
+  // useEffect(() => {
+  //   const docRef = doc(db, "users", `${user?.email}`);
+  //   console.log(docRef);
+  //   const docSnap = await getDoc(docRef);
+  //   console.log(docSnap);
+  // }, [user.email]);
+  // useEffect(() => {
+  //   onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+  //     console.log(doc.data.faveList);
+  //     setFaveList(doc.data().faveList);
+  //   });
+  // }, [user.email]);
+
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersRef);
+  //     console.log(data);
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data() })));
+  //   };
+  //   getUsers();
+  // }, []);
   return (
     <div className="account-page">
       <div className="account-page__details">
@@ -50,6 +86,9 @@ const Account = () => {
           <button className="signout-button">Watch List</button>
           <button className="signout-button">3x3</button>
         </div>
+      </div>
+      <div className="profile-lists">
+        <div className="show-card"></div>
       </div>
     </div>
   );
