7e536ec93396af394edd23dba507de517fc9020b
Richard Chiu <richard.chiu87@gmail.com>
2023-06-19 13:54:21 -0400

    nav mobile styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,9 +12,13 @@ import { UserAuth } from "../../context/AuthContext";
 const NavBar = () => {
   const { user, logOut } = UserAuth();
   const [activeNavLink, setActiveNavLink] = useState("");
-
+  const [isOpen, setIsOpen] = useState(false);
   const handleNavLinkClick = (navLink) => {
     setActiveNavLink(navLink);
+  };
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
   };
 
   const navigate = useNavigate();
@@ -28,74 +32,265 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="navBar">
-      <ul className="navBar__links">
-        <div className="navBar__sep">
-          <Link className="logo-link" to="/">
-            <img className="logo" src={logo} alt="site logo"></img>
-            <h1 className="site__title">STREAMIFY</h1>
-          </Link>
-          <div>
-            <li>
-              <NavLink
-                className={`navBar__anchor ${
-                  activeNavLink === "home" ? "active" : ""
+    <header className=" relative">
+      <button
+        className="flex-col justify-center items-center lg:hidden p-8"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-white block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
+      </button>
+      {isOpen ? (
+        <div
+          className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+      bg-black/60 rounded-lg backdrop-blur-md py-32 lg:hidden"
+        >
+          <nav>
+            <NavLink
+              className="flex"
+              to="/"
+              onClick={() => handleNavLinkClick("home")}
+            >
+              <img
+                className="navBar__icons-mobile mt-1"
+                src={home}
+                alt="home icon"
+              ></img>{" "}
+              <span
+                className={` ${
+                  activeNavLink === "home"
+                    ? "active navBar__anchor-mobile"
+                    : "navBar__anchor-mobile"
                 }`}
-                to="/"
-                onClick={() => handleNavLinkClick("home")}
               >
-                <img className="navBar__icons" src={home} alt="home icon"></img>
                 Home
-              </NavLink>
-            </li>
-            <li>
+              </span>
+            </NavLink>
+            <NavLink
+              className={` ${
+                activeNavLink === "search"
+                  ? "active navBar__anchor-mobile"
+                  : "navBar__anchor-mobile"
+              }`}
+              to="/search"
+              onClick={() => handleNavLinkClick("search")}
+            >
+              {" "}
+              <img
+                className="navBar__icons-mobile mt-1"
+                src={search}
+                alt="search icon"
+              ></img>{" "}
+              Search
+            </NavLink>
+            <NavLink
+              className={` ${
+                activeNavLink === "chat"
+                  ? "active navBar__anchor-mobile mr-4"
+                  : "navBar__anchor-mobile mr-4"
+              }`}
+              to="/chat"
+              onClick={() => handleNavLinkClick("chat")}
+            >
+              {" "}
+              <img
+                className="navBar__icons-mobile mt-1"
+                src={chat}
+                alt="chat icon"
+              ></img>{" "}
+              Chat
+            </NavLink>
+            {/* <NavLink
+              className={` ${
+                activeNavLink === "3x3"
+                  ? "active block mb-2"
+                  : "block text-white mb-2"
+              }`}
+              to="/3x3"
+              onClick={() => handleNavLinkClick("chat")}
+            >
+              3x3
+            </NavLink> */}
+          </nav>
+          <nav className="mt-8 w-full flex justify-center items-center">
+            {user ? (
+              <div className="flex justify-between w-[65%]">
+                <div>
+                  <NavLink
+                    className="navBar__anchor-mobile--2"
+                    to="/account"
+                    onClick={() => setActiveNavLink("")}
+                  >
+                    <img
+                      className="navBar__icons-mobile"
+                      src={userIcon}
+                      alt="login icon"
+                    ></img>
+                    Profile
+                  </NavLink>
+                </div>
+                <div>
+                  <div
+                    className="navBar__anchor-mobile--2"
+                    to="/account"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </div>
+                </div>
+              </div>
+            ) : (
               <NavLink
-                className={`navBar__anchor ${
-                  activeNavLink === "search" ? "active" : ""
-                }`}
-                to="/search"
-                onClick={() => handleNavLinkClick("search")}
+                // className="navBar__anchor"
+                className="navBar__anchor-mobile mr-3"
+                to="/login"
+                onClick={() => setActiveNavLink("")}
               >
-                <img
-                  className="navBar__icons"
-                  src={search}
-                  alt="search icon"
-                ></img>
-                Search
+                {
+                  <img
+                    className="navBar__icons"
+                    src={userIcon}
+                    alt="login icon"
+                  ></img>
+                }
+                Login
               </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={`navBar__anchor ${
-                  activeNavLink === "chat" ? "active" : ""
-                }`}
-                to="/chat"
-                onClick={() => handleNavLinkClick("chat")}
-              >
-                <img className="navBar__icons" src={chat} alt="chat icon"></img>
-                Chat
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                className={`navBar__anchor ${
-                  activeNavLink === "3x3" ? "active" : ""
-                }`}
-                to="/3x3"
-                onClick={() => handleNavLinkClick("3x3")}
-              >
-                <img className="navBar__icons" src={grid} alt="list icon"></img>
-                3 x 3
-              </NavLink>
-            </li>
-          </div>
+            )}
+          </nav>
         </div>
-        {user ? (
-          <div className="account-tab">
-            <div>
+      ) : null}
+      <div className="hidden lg:block">
+        <nav className="navBar">
+          <ul className="navBar__links">
+            <div className="navBar__sep">
+              <Link className="logo-link" to="/">
+                <img
+                  className="lg:h-[2.5rem] lg:mr-4 lg:ml-6 xl:h-[3.5rem]"
+                  src={logo}
+                  alt="site logo"
+                ></img>
+                <h1 className="site__title lg:text-[2.5rem] xl:text-[3rem]">
+                  STREAMIFY
+                </h1>
+              </Link>
+              <div>
+                <li>
+                  <NavLink
+                    className={`navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px] ${
+                      activeNavLink === "home"
+                        ? "active lg:p-[10px] xl:p-[18px]"
+                        : ""
+                    }`}
+                    to="/"
+                    onClick={() => handleNavLinkClick("home")}
+                  >
+                    <img
+                      className="navBar__icons"
+                      src={home}
+                      alt="home icon"
+                    ></img>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={`navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px] ${
+                      activeNavLink === "search"
+                        ? "active lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                        : ""
+                    }`}
+                    to="/search"
+                    onClick={() => handleNavLinkClick("search")}
+                  >
+                    <img
+                      className="navBar__icons"
+                      src={search}
+                      alt="search icon"
+                    ></img>
+                    Search
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={`navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px] ${
+                      activeNavLink === "chat"
+                        ? "active lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                        : ""
+                    }`}
+                    to="/chat"
+                    onClick={() => handleNavLinkClick("chat")}
+                  >
+                    <img
+                      className="navBar__icons"
+                      src={chat}
+                      alt="chat icon"
+                    ></img>
+                    Chat
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className={`navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px]${
+                      activeNavLink === "3x3"
+                        ? "active lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                        : ""
+                    }`}
+                    to="/3x3"
+                    onClick={() => handleNavLinkClick("3x3")}
+                  >
+                    <img
+                      className="navBar__icons"
+                      src={grid}
+                      alt="list icon"
+                    ></img>
+                    3 x 3
+                  </NavLink>
+                </li>
+              </div>
+            </div>
+            {user ? (
+              <div className="account-tab">
+                <div>
+                  <NavLink
+                    className="navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                    to="/account"
+                    onClick={() => setActiveNavLink("")}
+                  >
+                    <img
+                      className="navBar__icons"
+                      src={userIcon}
+                      alt="login icon"
+                    ></img>
+                    Profile
+                  </NavLink>
+                </div>
+                <div>
+                  <div
+                    className="navBar__anchor lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                    to="/account"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </div>
+                </div>
+              </div>
+            ) : (
               <NavLink
-                className="navBar__anchor"
-                to="/account"
+                className="navBar__anchor login lg:p-[10px] xl:p-[18px] xl:text-[18px]"
+                to="/login"
                 onClick={() => setActiveNavLink("")}
               >
                 <img
@@ -103,35 +298,13 @@ const NavBar = () => {
                   src={userIcon}
                   alt="login icon"
                 ></img>
-                Profile
+                Login
               </NavLink>
-            </div>
-            <div>
-              <div
-                className="navBar__anchor"
-                to="/account"
-                onClick={handleSignOut}
-              >
-                Sign Out
-              </div>
-            </div>
-          </div>
-        ) : (
-          <NavLink
-            className="navBar__anchor login"
-            to="/login"
-            onClick={() => setActiveNavLink("")}
-          >
-            <img
-              className="navBar__icons"
-              src={userIcon}
-              alt="login icon"
-            ></img>
-            Login
-          </NavLink>
-        )}
-      </ul>
-    </nav>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,6 @@
 import "./HomePage.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/icons/streamify-logo.svg";
 import HomeMessage from "../../components/HomeMessage/HomeMessage";
 import NavBar from "../../components/NavBar/NavBar";
 import screens from "../../assets/images/screens.svg";
@@ -6,9 +8,19 @@ import Footer from "../../components/Footer/Footer";
 const HomePage = () => {
   return (
     <>
-      <div className="homePage">
+      <div className="homePage relative">
         <NavBar />
         <div className="homePage-main">
+          {/* <Link className="logo-link absolute" to="/">
+            <img
+              className="xs:h-[1.5rem] lg:h-[2.5rem] lg:mr-4 lg:ml-6 xl:h-[3.5rem]"
+              src={logo}
+              alt="site logo"
+            ></img>
+            <h1 className="site__title sm:text-[2.5rem] xl:text-[3rem]">
+              STREAMIFY
+            </h1>
+          </Link> */}
           <HomeMessage className="homePage-textContainer" />
           <img className="home-screen" src={screens}></img>
         </div>
