7d2b0ece1bde511da420b0b9b5fd79c51c2dd3e3
Richard Chiu <richard.chiu87@gmail.com>
2023-06-25 00:35:37 -0400

    log-in mobile add

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -39,47 +39,101 @@ const LoginPage = () => {
   return (
     <div className="signup-page">
       <img className="signup-img" src={phoneScreens} />
-      <div className="signup-container lg:relative flex flex-col">
-        <form onSubmit={handleSubmit} className="signup-form login-form">
-          <h1 className="signup-title login-title">Log in</h1>
+      <div className=" lg:hidden">
+        <div className="signup-container flex flex-col lg:w-[37.5rem] lg:h-[25rem]">
+          <form
+            onSubmit={handleSubmit}
+            className="signup-form login-form lg:p-9 p-5 pb-0 lg:h-[25rem]"
+          >
+            <h1 className="signup-title login-title">Log in</h1>
 
-          <input
-            className={`signup-input ${errorMessage && "error-input"}`}
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+            <input
+              className={`signup-input ${errorMessage && "error-input"}`}
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <input
-            className={`signup-input ${errorMessage && "error-input"}`}
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            <input
+              className={`signup-input ${errorMessage && "error-input"}`}
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-          {errorMessage && <div className="error-message">{errorMessage}</div>}
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
 
-          <p className="login-message">
-            Don't have an account yet?
-            <Link to="/signup">
-              <span className="login-link"> Sign up</span>
-            </Link>
-          </p>
-          <button className="signup-button">Login </button>
-        </form>
-        <div className="right">
-          <div className="social-buttons-container">
-            <GoogleButton onClick={handleGoogleSignIn} />
+            <p className="login-message">
+              Don't have an account yet?
+              <Link to="/signup">
+                <span className="login-link"> Sign up</span>
+              </Link>
+            </p>
+            <button className="signup-button">Login </button>
+          </form>
+          <div className="right lg:h-[25rem] p-5">
+            <div className="social-buttons-container lg:mt-[8rem]">
+              <GoogleButton onClick={handleGoogleSignIn} />
+            </div>
           </div>
+          {/* <div className="or">OR</div> */}
         </div>
-        {/* <div className="or">OR</div> */}
       </div>
 
       <img className="signup-img2" src={phoneScreens} />
+
+      <div className="hidden lg:block">
+        <div className="signup-container lg:w-[37.5rem] lg:h-[25rem]">
+          <form
+            onSubmit={handleSubmit}
+            className="signup-form login-form lg:p-9 p-5 pb-0 lg:h-[25rem] lg:absolute"
+          >
+            <h1 className="signup-title login-title">Log in</h1>
+
+            <input
+              className={`signup-input ${errorMessage && "error-input"}`}
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <input
+              className={`signup-input ${errorMessage && "error-input"}`}
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {errorMessage && (
+              <div className="error-message">{errorMessage}</div>
+            )}
+
+            <p className="login-message">
+              Don't have an account yet?
+              <Link to="/signup">
+                <span className="login-link"> Sign up</span>
+              </Link>
+            </p>
+            <button className="signup-button">Login </button>
+          </form>
+          <div className="right lg:h-[25rem] lg:absolute lg:p-10">
+            <div className="lg:mt-[8.5rem]">
+              <GoogleButton onClick={handleGoogleSignIn} />
+            </div>
+          </div>
+          <div className="or">OR</div>
+        </div>
+      </div>
     </div>
   );
 };
